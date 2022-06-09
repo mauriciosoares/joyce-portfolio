593ea29bf8e6c5b0bf8e99562b5df3895cd0e814
Mauricio Soares <m.soares2603@gmail.com>
2022-06-09 17:25:20 -0300

    add image url for sharing

--- src/scripts.js
+++ src/scripts.js
@@ -1,55 +1,74 @@
-let currentImageId = null
-let imageIds = []
+let currentImageId = null;
+let imageIds = [];
+let modalIsOpen = false;
 
-let modalImageNode = document.querySelector('[data-modal-image]')
+let modalImageNode = document.querySelector("[data-modal-image]");
 
 function closeModal() {
-  modalImageNode.innerHTML = ''
-  document.body.classList.remove('show-modal')
+  modalImageNode.innerHTML = "";
+  document.body.classList.remove("show-modal");
+  modalIsOpen = false;
+  history.pushState(null, null, " ");
 }
 
-document.querySelector('[data-modal-close]').addEventListener('click', closeModal)
+document
+  .querySelector("[data-modal-close]")
+  .addEventListener("click", closeModal);
 
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') closeModal()
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") closeModal();
 
-  if (e.key === 'ArrowRight') document.querySelector('[data-gallery-nav="next"]').click()
-  if (e.key === 'ArrowLeft') document.querySelector('[data-gallery-nav="prev"]').click()
-})
+  if (e.key === "ArrowRight" && modalIsOpen)
+    document.querySelector('[data-gallery-nav="next"]').click();
+  if (e.key === "ArrowLeft" && modalIsOpen)
+    document.querySelector('[data-gallery-nav="prev"]').click();
+});
 
 function setModalImage(imageId) {
-  const img = document.querySelector(`[data-image-id="${imageId}"]`)
-  document.body.classList.add('show-modal')
-  currentImageId = imageId
+  history.pushState(null, null, `#${imageId}`);
+  const img = document.querySelector(`[data-image-id="${imageId}"]`);
+  document.body.classList.add("show-modal");
+  modalIsOpen = true;
+  currentImageId = imageId;
 
-  const imageUrl = new URL(`/images/gallery/full/${imageId}.jpg`, import.meta.url).href
+  const imageUrl = new URL(
+    `/images/gallery/full/${imageId}.jpg`,
+    import.meta.url
+  ).href;
 
-  modalImageNode.style.backgroundImage = `url(${imageUrl})`
+  modalImageNode.style.backgroundImage = `url(${imageUrl})`;
 }
 
 Array.from(document.querySelectorAll("[data-image-id]")).forEach((img) => {
   const { imageId } = img.dataset;
-  imageIds.push(imageId)
+  imageIds.push(imageId);
 
   img.addEventListener("click", () => {
-    setModalImage(imageId)
+    setModalImage(imageId);
   });
 });
-
 
 Array.from(document.querySelectorAll("[data-gallery-nav]")).forEach((img) => {
   img.addEventListener("click", () => {
     const { galleryNav } = img.dataset;
-    let nextImage
-    
-    if (galleryNav === 'next') {
-      nextImage = imageIds[imageIds.indexOf(currentImageId) + 1] || imageIds[0]
+    let nextImage;
+
+    if (galleryNav === "next") {
+      nextImage = imageIds[imageIds.indexOf(currentImageId) + 1] || imageIds[0];
       // get indexOf +1
       // if undefined, show on index 0
     } else {
-      nextImage = imageIds[imageIds.indexOf(currentImageId) - 1] || imageIds[imageIds.length - 1]
+      nextImage =
+        imageIds[imageIds.indexOf(currentImageId) - 1] ||
+        imageIds[imageIds.length - 1];
     }
 
-    setModalImage(nextImage)
+    setModalImage(nextImage);
   });
+});
+
+document.addEventListener("readystatechange", () => {
+  if (window.location.hash) {
+    setModalImage(window.location.hash.replace("#", ""));
+  }
 });
